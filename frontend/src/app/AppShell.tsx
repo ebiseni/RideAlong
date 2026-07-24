bd31d1462bb2249958f0fa686705976659373e72
Chukwuma Henry <fadalurd08@gmail.com>
2026-07-24 13:56:51 +0100

    fix: reconfigured the AppShell

--- frontend/src/app/AppShell.tsx
+++ frontend/src/app/AppShell.tsx
@@ -1,17 +1,11 @@
 import { Outlet } from "react-router-dom";
 import Sidebar from "../components/shared/Sidebar";
-import sidebarProfileImage from "../assets/images/sidebar-profile-image.jpg";
 import "../styles/components/layout/AppShell.css";
 
 export default function AppShell() {
   return (
     <div className="app-shell">
-      {/* TEMP: hardcoded placeholder values until AuthContext/useAuth is implemented */}
-       <Sidebar
-        userName="Precious Aree"
-        userEmail="preciousaree56@gmail.com"
-        userAvatarUrl={sidebarProfileImage}
-      />
+      <Sidebar />
       <main className="app-shell-content">
         <Outlet />
       </main>
